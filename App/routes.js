7632a5dfdc3e5bd0eb316fb093b281dc45e4a586
Jeff "Huey" Huelsbeck <jhuelsbeck@cvent.com>
2015-09-23 16:50:07 -0400

    Remove spaces

--- App/routes.js
+++ App/routes.js
@@ -5,10 +5,10 @@ import { Layout, Home, NotFound, Profile, Login } from './pages';
 
 const routes = (
   <Route path="/" component={Layout}>
-    <IndexRoute component={Home} />
-    <Route path="login" component={Login} />
-    <Route path="profile/:username" component={Profile} />
-    <Route path="*" component={NotFound} />
+    <IndexRoute component={Home}/>
+    <Route path="login" component={Login}/>
+    <Route path="profile/:username" component={Profile}/>
+    <Route path="*" component={NotFound}/>
   </Route>
 );
 
