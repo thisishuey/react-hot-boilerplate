99f629711183ede3127298d128415d762ee8cf59
Jeff "Huey" Huelsbeck <jhuelsbeck@cvent.com>
2015-09-23 01:47:12 -0400

    Add react router functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Radium, { Style } from 'radium';
-import App from './App';
+import Router, { Route, Link, DefaultRoute, RouteHandler } from 'react-router';
 
-React.render(<App />, document.getElementById('app'));
+import style from './style';
+import rules from './rules';
+
+import Home from './Home';
+import Login from './Login';
+import Container from './Container';
+
+require('bootstrap/less/bootstrap.less');
+require('font-awesome/less/font-awesome.less');
+
+@Radium
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+        <RouteHandler/>
+        <Style rules={rules}/>
+      </Container>
+    );
+  }
+};
+
+const routes = (
+  <Route path="/" handler={App}>
+    <Route path="home" handler={Home}/>
+    <Route path="login" handler={Login}/>
+  </Route>
+);
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.body);
+});
---
+++ src/style.js
@@ -0,0 +1,15 @@
+export default {
+  base: {
+    backgroundColor: 'gray',
+    color: 'white',
+    ':hover': {
+      backgroundColor: 'red'
+    },
+    ':focus': {
+      backgroundColor: 'green'
+    },
+    ':active': {
+      backgroundColor: 'yellow'
+    }
+  }
+};
